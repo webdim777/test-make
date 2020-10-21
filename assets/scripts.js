1347ce08ca972b57a0a96158b81fa595fb045b63
webdim777 <gitwebdim7@gmail.com>
2020-10-21 14:16:15 +0300

    product

--- assets/scripts.js
+++ assets/scripts.js
@@ -12,5 +12,63 @@ document.querySelectorAll('.search-btn').forEach(function(mobToggleItem) {
         document.querySelector('.header__search').classList.toggle('header__search__open');
     };
 });
+var prodGallery = new Swiper('.product-gallery', {
+    loop: true,
+    navigation: {
+        nextEl: '.gallery_next',
+        prevEl: '.gallery_prev',
+    },    
+})
+var prodGallerythumbs = new Swiper('.product-gallery-thumbs', {
+    slidesPerView: 3,
+    spaceBetween: 8,
+    breakpoints: {
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 16
+      }
+    },
+    navigation: {
+      nextEl: '.thumbs_next',
+      prevEl: '.thumbs_prev',
+    },
+  })
+  prodGallery.on("transitionEnd", function(e) {
+    document.querySelectorAll('.js-gallery-trigger').forEach((el) => {
+      el.classList.remove("is-active");
+    });
+    document.getElementsByClassName('js-gallery-trigger')[prodGallery.activeIndex].classList.add("is-active");
+    if (prodGallerythumbs && prodGallerythumbs.swiper) prodGallerythumbs.swiper.slideTo(prodGallery.activeIndex);
+  });
+  prodGallery.params.control = prodGallerythumbs;
+  prodGallerythumbs.params.control = prodGallery;
+
+var buttonItems = document.querySelectorAll('.js-gallery-trigger'), index, button;
+for (index = 0; index < buttonItems.length; index++) {
+    button = buttonItems[index];
+    button.addEventListener('click', function (event) {
+      var index = Array.prototype.indexOf.call(document.querySelectorAll('.js-gallery-trigger'), event.target);
+      if (prodGallery && prodGallery.swiper) {
+        prodGallery.swiper.slideTo(index);
+      }
+    });
+}
+
+
+    var plus = document.getElementsByClassName('quantity_plus')[0];
+    var minus = document.getElementsByClassName('quantity_minus')[0];
+    var field = document.getElementsByClassName('quantity-input__element')[0];
+    
+    plus.addEventListener('click', function(){ 
+      field.value = Calc(1); 
+    });
+    minus.addEventListener('click', function(){
+      field.value = Calc(-1);  
+    });
+    
+    function Calc(num) {
+      let result = parseInt(field.value) + num; 
+      return (result < 1) ? 1 : result;
+    }
 /******/ })()
 ;
